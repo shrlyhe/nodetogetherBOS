6cc37c36d4d9eb523840bb6d0d57932cb8a6fd6a
shrlyhe <she@wesleyan.edu>
2016-08-23 21:06:46 -0400

    add server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-require('express');
+var port = process.env.PORT || '8080';
+var host = process.env.HOST || '0.0.0.0';
+var express = require('express');
+
+var app = express();
+
+
+app.listen(port,host);
+
+console.log('Server running %s:%d...', host, port);
